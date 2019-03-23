24fe7afd6ad5b4ead227929080d1f97f927a4943
purupuru <keisamurai@gmail.com>
2019-03-23 23:13:30 +0900

    log

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')_^^%v(8wshub=1#2@i2@(ii#-!o2tl9u*us(0_@se!lnxao04'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["lbeacon.azurewebsites.net", "127.0.0.1"]
 
